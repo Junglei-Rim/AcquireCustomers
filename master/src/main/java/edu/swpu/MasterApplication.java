ee390849ae7954b2b0e0bd15903853e2d3cfbadf
GuoZhenglei <3075114008@qq.com>
2024-03-26 17:55:10 +0800

    添加五个模块：网关模块，master模块管理依赖，数据处理模块，客户识别模块，推广策略模块

---
+++ master/src/main/java/edu/swpu/MasterApplication.java
@@ -0,0 +1,13 @@
+package edu.swpu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MasterApplication.class, args);
+    }
+
+}
---
+++ strategy/src/main/java/edu/swpu/StrategyApplication.java
@@ -0,0 +1,13 @@
+package edu.swpu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrategyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StrategyApplication.class, args);
+    }
+
+}
